b9d8f6dc2e733c66f867d57b7ee4b950380f2782
Brandon M. Scrushy <brandon@zcorp.digital>
2020-07-24 12:36:19 -0500

    Issues w Airtable and Select Options... any ideas?

--- frontend/index.js
+++ frontend/index.js
@@ -1,17 +1,64 @@
-import {initializeBlock, FormField, Input } from '@airtable/blocks/ui'
+import {initializeBlock, FormField, Input, Box, Select, useBase, useRecords, Text} from '@airtable/blocks/ui'
 import React, {useState} from 'react'
 
-function HelloWorldBlock() {
+function HueHelperBlock() {
+    // Hooks & Stuff
+const base = useBase()
+const table = base.getTableByName('hue')
+const records = useRecords(table)
+
+    // Choices
+const rooms = (table.getFieldByName('room')).options.choices
+const roomChoices = Array.from(rooms.map(room => room.name))
+
+    // State
 const [serialNum, setSerialNum] = useState('')
+const [lightName, setLightName] = useState('')
+const [lightRoom, setLightRoom] = useState('')
+
+function checkChoices() {
+    console.log(roomChoices)
+}
+
+checkChoices()
 
     return(
-        <div>
-            <h1>Hue Helper</h1>
-            <FormField label="Serial #">
-                <Input value={serialNum} onChange={e => setSerialNum(e.target.value)} />
-            </FormField>
-        </div>
+<div>
+    <div className="row" style={{display: 'flex', flexDirection: 'row', justifyContent: 'center', alignContent: 'center', flexWrap: 'wrap'}}>
+        <h2>Add Light</h2>
+    </div>
+
+    <div className="row" style={{display: 'flex', flexDirection: 'row', justifyContent: 'center', alignContent: 'center', width: '100%'}}>
+        <Box
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            border="default"
+            backgroundColor="lightGray1"
+            borderRadius="large"
+            padding={1}
+            width={300}
+            height={200}
+            overflow="hidden"
+        >
+            <form>
+                <FormField label="Serial #">
+                    <Input value={serialNum} onChange={e => setSerialNum(e.target.value)} style={{width: '100%'}} />
+                </FormField>
+                <FormField label="Name">
+                    <Input value={lightName} onChange={e => setLightName(e.target.value)} style={{width: '100%'}} />
+                </FormField>
+                <FormField label="Room">
+                    <Select style={{width: '100%'}} options={roomChoices}>
+                    </Select>
+
+                </FormField>
+            </form>
+        </Box>
+
+    </div>
+</div>
     )
 }
 
-initializeBlock(() => <HelloWorldBlock />);
+initializeBlock(() => <HueHelperBlock />);
